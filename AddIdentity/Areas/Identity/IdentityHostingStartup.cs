9e2c8881ef846c1354a57decd363ca1ca5efd9e0
Dobri Zhekov <zhekovdobri@gmail.com>
2022-04-14 10:58:30 +0200

    first commit

---
+++ AddIdentity/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using AddIdentity.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AddIdentity.Areas.Identity.IdentityHostingStartup))]
+namespace AddIdentity.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AddIdentityContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AddIdentityContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<AddIdentityContext>();
+            });
+        }
+    }
+}
---
+++ AddIdentity/Areas/Identity/TokenProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AddIdentity.Areas.Identity
+{
+    public class TokenProvider
+    {
+        public string XsrfToken { get; set; }
+        public string RefreshToken { get; set; }
+    }
+}
